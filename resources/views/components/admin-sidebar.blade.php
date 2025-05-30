63ecc51da874d13db1705820079225a96981da6a
basnetdebit <basnetdebit2@gmail.com>
2025-05-30 09:05:17 +0545

    Simpal Demo Dashboard

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+    <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">DEBIT</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="dropdown active">
+              <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="briefcase"></i><span>Widgets</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
+                <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
+              </ul>
+            </li>
+          </ul>
+        </aside>
+</div>
